9f9a40b86150142283bf907acf760978798ec9a0
Jonathan Odeldahl <airaktai@gmail.com>
2020-11-25 14:51:18 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# TwoDo 
+
+## This small app is created for the sole purpose of being a exercise
+
+## Get to know Entity Framework Core and MS SQL
